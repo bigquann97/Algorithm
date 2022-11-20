4c6a81e7edc9f051223bae1f83948f9faff4004c
Gwanho KIM <bigquan977@gmail.com>
2022-11-21 03:00:04 +0900

    commit

--- src/Programmers.java
+++ src/Programmers.java
@@ -2,6 +2,10 @@ public class Programmers {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
+        String s = "one4seveneight";
+
+        int solution1 = solution.solution(s);
+        System.out.println("solution1 = " + solution1);
 
     }
 }
