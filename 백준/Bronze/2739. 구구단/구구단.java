2ad31cc437967271a61e66db0d5087dd3c95b94e
Gwanho KIM <bigquann97@khu.ac.kr>
2022-11-20 17:12:43 +0900

    [Bronze V] Title: 구구단, Time: 160 ms, Memory: 16264 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < 9; i++) {
+            bw.write(a + " * " + (i + 1) + " = " + a * (i + 1) + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
