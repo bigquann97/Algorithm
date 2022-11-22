5ca9a018cfe6a057bc3a47b36d7e7b7f6cd93c63
Gwanho KIM <bigquann97@khu.ac.kr>
2022-11-22 09:01:44 +0900

    [level 1] Title: 소수 만들기, Time: 9.22 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        for(int i = 0; i < nums.length-2; i++) {
+            for(int j = i+1; j < nums.length-1; j++) {
+                for(int k = j+1; k < nums.length; k++) {
+                    if(isSosu(nums[i] + nums[j] + nums[k])) {
+                        System.out.println(""+ i + j + k);
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+    public boolean isSosu(int n) {
+        if (n == 1)
+            return false;
+        if (n == 2)
+            return true;
+        for(int i = 2; i <= Math.sqrt(n); i++) {
+            if(n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
