0fc43e070cbfacb17e9b3fe5057507a510d7ce2f
Gwanho KIM <bigquann97@khu.ac.kr>
2022-12-12 04:34:37 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 2.71 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/README.md
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열 내 마음대로 정렬하기 - 12915 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12915) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12915?language=java) 
 
 ### 성능 요약
 
-메모리: 77.4 MB, 시간: 3.45 ms
+메모리: 74.2 MB, 시간: 2.71 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -1,26 +1,9 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 class Solution {
     public String[] solution(String[] strings, int n) {
-        String[] answer = new String[strings.length];
-
-        Map<String, Character> map = new HashMap<>();
-
-        for (int i = 0; i < strings.length; i++) {
-            map.put(strings[i], strings[i].charAt(n));
-        }
-
-        List<String> keySet = map.keySet().stream().sorted().collect(Collectors.toList());
-
-        List<String> collect = keySet.stream().sorted(Comparator.comparing(map::get)).collect(Collectors.toList());
-
-        
-        
-        for (int i = 0; i < strings.length; i++) {
-            answer[i] = collect.get(i);
-        }
-
-        return answer;
+        Arrays.sort(strings);
+        Arrays.sort(strings, Comparator.comparing(x -> x.charAt(n)));
+        return strings;
     }
 }
