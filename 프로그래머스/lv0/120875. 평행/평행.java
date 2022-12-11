1a1cdde59ceeda7b90fd5c542b05b0c7625bc3c9
Gwanho KIM <bigquann97@khu.ac.kr>
2022-12-12 03:20:56 +0900

    [level 0] Title: 평행, Time: 0.03 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/평행.java
+++ 프로그래머스/lv0/120875. 평행/평행.java
@@ -1,23 +1,32 @@
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 class Solution {
     public int solution(int[][] dots) {
         int answer = 0;
-        loop1:
-        for (int i = 0; i < dots.length - 1; i++) {
-            for (int j = i + 1; j < dots.length; j++) {
-                List<Integer> lst = new ArrayList<>(Arrays.asList(0, 1, 2, 3));
-                lst.remove(Integer.valueOf(i));
-                lst.remove(Integer.valueOf(j));
-                if ((double) (dots[i][1] - dots[j][1]) / (dots[i][0] - dots[j][0]) ==
-                        (double) (dots[lst.get(0)][1] - dots[lst.get(1)][1]) / (dots[lst.get(0)][0] - dots[lst.get(1)][0])) {
-                    answer++;
-                    break loop1;
+
+        ArrayList<Double> giList = new ArrayList<>();
+
+        for(int i = 0; i < 4; i ++) {
+            for (int j = i + 1; j < 4; j++) {
+                Double gi = 0.0d;
+                if (dots[i][0] < dots[j][0]) {
+                    gi = (double) (dots[j][1] - dots[i][1]) / (double) (dots[j][0] - dots[i][0]);
+                    giList.add(gi);
+                } else {
+                    gi = (double) (dots[i][1] - dots[j][1]) / (double) (dots[i][0] - dots[j][0]);
+                    giList.add(gi);
                 }
             }
         }
-        return answer;
+
+        for (int i = 0; i < giList.size() - 1; i++) {
+            for (int j = i + 1; j < giList.size(); j++) {
+                if (giList.get(i).equals(giList.get(j))) {
+                    return 1;
+                }
+            }
+        }
+        
+        return 0;
     }
 }
