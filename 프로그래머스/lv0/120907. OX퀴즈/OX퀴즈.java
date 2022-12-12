06a0295d8a52c5a111445ec7d95f0cbceb8949fe
Gwanho KIM <bigquann97@khu.ac.kr>
2022-12-12 14:18:22 +0900

    [level 0] Title: OX퀴즈, Time: 0.49 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
@@ -4,27 +4,20 @@ class Solution {
         int i = 0;
         for (String s : quiz) {
             String sik = s.split(" = ")[0];
-            Integer dap = Integer.valueOf(s.split(" = ")[1]);
+            String dapStr = s.split(" = ")[1];
 
             if (sik.contains(" - ")) {
                 int a = Integer.parseInt(sik.split(" - ")[0]);
                 int b = Integer.parseInt(sik.split(" - ")[1]);
-                if (a - b == dap) {
-                    answer[i++] = "O";
-                } else{
-                    answer[i++] = "X";
-                }
+                int dap = Integer.parseInt(dapStr);
+                answer[i++] = a - b == dap ? "O" : "X";
             } else {
                 int a = Integer.parseInt(sik.split(" \\+ ")[0]);
                 int b = Integer.parseInt(sik.split(" \\+ ")[1]);
-                if (a + b == dap) {
-                    answer[i++] = "O";
-                } else{
-                    answer[i++] = "X";
-                }
+                int dap = Integer.parseInt(dapStr);
+                answer[i++] = a + b == dap ? "O" : "X";
             }
         }
-
         return answer;
     }
 }
