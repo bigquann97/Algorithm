4411cad2fd25a8407cee533f6449284a450060fa
Gwanho KIM <bigquann97@khu.ac.kr>
2022-12-13 15:12:22 +0900

    [level 0] Title: 안전지대, Time: 0.02 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.java
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.java
@@ -1,10 +1,12 @@
+
 class Solution {
     public int solution(int[][] board) {
         int answer = 0;
-        int[][] zone = new int[board.length][board[0].length]; // 0 으로 모두 초기화 - 0 safe 1 danger
+        int size = board.length;
+        int[][] zone = new int[size][size]; // 0 으로 모두 초기화 - 0 safe 1 danger
 
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 if (board[i][j] == 1) { // 폭탄 발견
                     zone[i][j]++;
 
@@ -17,26 +19,26 @@ class Solution {
                     if(i != 0 && j != 0)
                         zone[i - 1][j - 1]++;
 
-                    if(j != board.length - 1)
+                    if(j != size - 1)
                         zone[i][j + 1]++;
 
-                    if(i != board.length - 1)
+                    if(i != size - 1)
                         zone[i + 1][j]++;
 
-                    if(i != board.length - 1 && j != board.length - 1)
+                    if(i != size - 1&& j != size - 1)
                         zone[i + 1][j + 1]++;
 
-                    if(i != board.length - 1 && j != 0)
+                    if(i != size - 1 && j != 0)
                         zone[i + 1][j - 1]++;
 
-                    if(i != 0 && j != board.length - 1)
+                    if(i != 0 && j != size - 1)
                         zone[i - 1][j + 1]++;
                 }
             }
         }
 
-        for (int i = 0; i < zone.length; i++) {
-            for (int j = 0; j < zone[i].length; j++) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 if(zone[i][j] == 0) {
                     answer++;
                 }
