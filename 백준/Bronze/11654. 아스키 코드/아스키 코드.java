837c9d2bb2860ab9ff512cb52bb862a51f4de3e2
Gwanho KIM <bigquann97@khu.ac.kr>
2023-03-25 13:00:00 +0900

    [Bronze V] Title: 아스키 코드, Time: 128 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] chars = br.readLine().toCharArray();
+        System.out.println((int)chars[0]);
+    }
+}
