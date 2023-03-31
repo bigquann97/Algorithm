6d39aac0d73ec38f4a4d64b2feb0709e5915de59
Gwanho KIM <bigquann97@khu.ac.kr>
2023-04-01 02:26:07 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.68 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -2,18 +2,52 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] array) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i : array) {
-            map.put(i, map.getOrDefault(i, 0) + 1);
+        Map<Integer, Integer> map = new LinkedHashMap<>();
+        Arrays.sort(array);
+        Integer value = array[0];
+        int count = 0;
+        
+        for(int i = 0; i < array.length; i++) {
+            if(array[i] == value) {
+                count++;
+            } else {
+                map.put(array[i-1], count);
+                count = 1;
+                value = array[i];
+            }
+            
+            if(i == array.length - 1) {
+                map.put(value, count);
+            }
         }
-        List<Integer> list = new ArrayList<>(map.keySet());
-        list.sort((o1, o2) -> map.get(o2).compareTo(map.get(o1)));
-        if(map.size() == 1) {
-            return list.get(0);
-        } else if (map.get(list.get(0)) == map.get(list.get(1))) {
-            return -1;
-        } else {
-            return list.get(0);
+        
+        int maxValue = -1;
+        Set<Integer> keySet = map.keySet();
+        int answer = 0;
+        int loop = 0;
+        
+        for(Integer key : keySet) {
+            int k = key;
+            int v = map.get(key);
+            
+            if(v >= maxValue) {
+                maxValue = v;
+                answer = k;
+            }
         }
+        
+        int reCount = 0;
+        
+        for(Integer key : keySet) {
+            int v = map.get(key);            
+            if(v == maxValue) {
+                reCount++;
+            }
+            if(reCount >= 2) {
+                return -1;
+            }
+        }
+        
+        return answer;
     }
 }
